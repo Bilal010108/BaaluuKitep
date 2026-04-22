b56db3721186a8c96a249da60b96cdec6d7bbcf1
Bilal <bilal010108@gmail.com>
2026-04-23 02:54:13 +0600

    email

--- books/settings.py
+++ books/settings.py
@@ -220,8 +220,8 @@ CORS_ALLOW_HEADERS = [
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER ='zholdoshevmairambek@gmail.com'
+EMAIL_HOST_PASSWORD ='mehd jlqj mtci ehpx'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
